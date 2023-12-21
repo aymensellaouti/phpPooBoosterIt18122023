de443e349a69bf9592c59173931dc1fde786b267
aymensellaouti <aymen.sellaouti@gmail.com>
2023-12-21 09:52:19 +0100

    sprint 1 pokemon

---
+++ class/AttackPkemon.php
@@ -0,0 +1,91 @@
+<?php 
+
+class AttackPokemon {
+
+    public function __construct(
+        protected int $minAttack,
+        protected int $maxAttack,
+        protected int $specialAttack,
+        protected int $probabilitySpecialAttack,
+    ) {}
+
+        /**
+         * Get the value of minAttack
+         */ 
+        public function getMinAttack()
+        {
+                return $this->minAttack;
+        }
+
+        /**
+         * Set the value of minAttack
+         *
+         * @return  self
+         */ 
+        public function setMinAttack($minAttack)
+        {
+                $this->minAttack = $minAttack;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of maxAttack
+         */ 
+        public function getMaxAttack()
+        {
+                return $this->maxAttack;
+        }
+
+        /**
+         * Set the value of maxAttack
+         *
+         * @return  self
+         */ 
+        public function setMaxAttack($maxAttack)
+        {
+                $this->maxAttack = $maxAttack;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of specialAttack
+         */ 
+        public function getSpecialAttack()
+        {
+                return $this->specialAttack;
+        }
+
+        /**
+         * Set the value of specialAttack
+         *
+         * @return  self
+         */ 
+        public function setSpecialAttack($specialAttack)
+        {
+                $this->specialAttack = $specialAttack;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of probabilitySpecialAttack
+         */ 
+        public function getProbabilitySpecialAttack()
+        {
+                return $this->probabilitySpecialAttack;
+        }
+
+        /**
+         * Set the value of probabilitySpecialAttack
+         *
+         * @return  self
+         */ 
+        public function setProbabilitySpecialAttack($probabilitySpecialAttack)
+        {
+                $this->probabilitySpecialAttack = $probabilitySpecialAttack;
+
+                return $this;
+        }
+}
---
+++ class/Pokemon.php
@@ -0,0 +1,92 @@
+<?php 
+
+class Pokemon {
+
+    public function __construct(
+        protected string $name,
+        protected string $imageUrl,
+        protected int $hp,
+        protected AttackPokemon $attackPokemon,
+    ) {}
+
+
+        /**
+         * Get the value of name
+         */ 
+        public function getName()
+        {
+                return $this->name;
+        }
+
+        /**
+         * Set the value of name
+         *
+         * @return  self
+         */ 
+        public function setName($name)
+        {
+                $this->name = $name;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of imageUrl
+         */ 
+        public function getImageUrl()
+        {
+                return $this->imageUrl;
+        }
+
+        /**
+         * Set the value of imageUrl
+         *
+         * @return  self
+         */ 
+        public function setImageUrl($imageUrl)
+        {
+                $this->imageUrl = $imageUrl;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of hp
+         */ 
+        public function getHp()
+        {
+                return $this->hp;
+        }
+
+        /**
+         * Set the value of hp
+         *
+         * @return  self
+         */ 
+        public function setHp($hp)
+        {
+                $this->hp = $hp;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of attackPokemon
+         */ 
+        public function getAttackPokemon()
+        {
+                return $this->attackPokemon;
+        }
+
+        /**
+         * Set the value of attackPokemon
+         *
+         * @return  self
+         */ 
+        public function setAttackPokemon($attackPokemon)
+        {
+                $this->attackPokemon = $attackPokemon;
+
+                return $this;
+        }
+}
